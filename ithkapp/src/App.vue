128d0d8cbc1c64ab31084ca7c6a713c43986f086
Nick L <44630816+TheXXOs@users.noreply.github.com>
2022-06-09 20:59:50 +0930

    Footer + renaming NormalDiv
    Added a footer (fixed to bottom of screen, not page) for where the output will go and renamed NormalDiv to OptionBox.

--- ithkapp/src/App.vue
+++ ithkapp/src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <!--<img alt="Vue logo" src="./assets/logo.png">
   <MainBody/>-->
+  <div id="content">
   <h1>Ithkapp</h1>
   <div style="display: flex; flex-flow: row wrap; justify-content: center;">
-    <NormalDiv title="Concatenation" :options='["None","Type 1","Type 2"]' code="concat" @send-message="handleSendMessage"/>
-    <NormalDiv title="Version" :options='{"PRC":"Processual","CPT":"Completive"}' code="ver" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="3" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="4" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="5" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="6" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="7" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="8" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="9" @send-message="handleSendMessage"/>
-    <NormalDiv title="aaa" :options='["test","test2","test3"]' code="10" @send-message="handleSendMessage"/>
-    <NormalDiv title="Slot V Affixes" code="5afx" @send-message="handleSendMessage" type="affix"/>
-    <NormalDiv title="Root (Slot III)" code="root" @send-message="handleSendMessage" type="text"/>
+    <OptionBox title="Concatenation" :options='["None","Type 1","Type 2"]' code="concat" @send-message="handleSendMessage"/>
+    <OptionBox title="Version" :options='{"PRC":"Processual","CPT":"Completive"}' code="ver" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="3" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="4" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="5" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="6" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="7" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="8" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="9" @send-message="handleSendMessage"/>
+    <OptionBox title="aaa" :options='["test","test2","test3"]' code="10" @send-message="handleSendMessage"/>
+    <OptionBox title="Slot V Affixes" code="5afx" @send-message="handleSendMessage" type="affix"/>
+    <OptionBox title="Root (Slot III)" code="root" @send-message="handleSendMessage" type="text"/>
   </div>
-  <p>(Note: The affix slots & root slot will eventually be modified to be a definition-based selector)</p>
+  <!--(Note: The affix slots & root slot will eventually be modified to be a definition-based selector)-->
+  </div>
+  <div id="footer">
+    <p>{{ithkword.join("")}} /{{ipa}}/</p>
+    <p>{{gloss}}</p>
+  </div>
 </template>
 
 <script>
-import NormalDiv from './components/normaldiv.vue'
+import OptionBox from './components/optionbox.vue'
 
 export default {
   name: 'App',
   components: {
-    NormalDiv
+    OptionBox,
+  },
+  data() {
+    return {
+      // the following variables correspond to things visible on-screen
+      ithkword: ["a","b","c","","","","","",""], // ithkuil word, split into slots
+      gloss: "al.FA.bet", // gloss of word
+      ipa: "eɪ bi: si:" // IPA transcription
+    }
   },
   methods: {
     handleSendMessage(value,code) {
@@ -43,5 +57,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#content {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  bottom: 100px;
+  right: 0px;
+  overflow: auto;
+}
+#footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 100px;
+  width: 100%;
+  border-style: solid;
+  border-width: 1px;
+  border-color: black;
+  background-color: white;
+  overflow: hidden;
 }
 </style>
--- ithkapp/src/components/optionbox.vue
+++ ithkapp/src/components/optionbox.vue
@@ -1,19 +1,20 @@
 <template>
-  <div id="normaldiv">
+  <div class="optionbox" :class="OBclass">
     <p>{{title}}</p>
+    <p class="error"></p>
     <!-- The following depends on which TYPE of grammar option this is: -->
     <!-- If it's a simple text entry: -->
     <input v-if="type=='text'" v-model="text" @input="this.$emit('send-message',text,code)" placeholder="Enter..." maxlength=4/>
     <!-- If it's a list of text entries: -->
     <div v-else-if="type=='affix'">
-      <div v-for="(affix,index) in affixes" :key="affix">
-        <input v-model.lazy="affixes[index][0]" @input="this.$emit('send-message',affixes,code)" placeholder="Enter..." maxlength=3/>
+      <div v-for="(affix,index) in affixes" :key="affix"> <!-- For each affix/degree pair in the array "affixes", add a textbox and dropdown linked to each-->
+        <input v-model="affixes[index][0]" @input="this.$emit('send-message',affixes,code)" placeholder="Enter..." maxlength=3/>
         <select v-model="affixes[index][1]" @input="this.$emit('send-message',affixes,code)" style="display:inline-block">
           <option v-for="num in Array(10).keys()" :key="num">{{num}}</option>
         </select>
       </div>
-      <input type="button" value="Add" @click="affixes.push(['',0])"/>
-      <input type="button" value="Remove" @click="affixes.pop()"/>
+      <input type="button" value="Add" @click="affixes.push(['',0]); this.$emit('send-message',affixes,code)"/>
+      <input type="button" value="Remove" @click="affixes.pop(); this.$emit('send-message',affixes,code)"/>
     </div>
     <!-- Otherwise, assume it's a dropdown list: -->
     <select v-else v-model="option" @change="this.$emit('send-message',option,code)">
@@ -26,18 +27,27 @@
 
 <script>
 export default {
-  name: 'NormalDiv',
+  name: 'OptionBox',
   props: {
     options: Array,
     title: String,
     code: String,
-    type: String
+    type: String,
   },
   data() {
     return {
       text: "",
       option: "Select...",
-      affixes: [["",0]]
+      affixes: [["",0]],
+      required: true
+    }
+  },
+  computed: {
+    OBclass() { // set class to error if the input is the default
+      console.log(this.affixes.every(function (e) {return e[0] != ""}));
+      return {
+        error: this.required & this.option == "Select..." && this.text == "" && this.affixes.length != 0 && !this.affixes.every(function (e) {return e[0] != ""})
+      }
     }
   }
 }
@@ -45,13 +55,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#normaldiv {
+.optionbox {
   float: left;
   border-style: solid;
   border-width: 1px;
   border-color: black;
   flex-grow: 1;
   flex-basis: 20vw;
+}
+.error {
+  border-color: red;
+  color: red;
 }
 h3 {
   margin: 40px 0 0;
