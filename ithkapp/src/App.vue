4fffc49644bc4f7c6d6614a0253d45008f6c7d02
Nick L <44630816+TheXXOs@users.noreply.github.com>
2022-06-13 12:25:31 +0930

    Added modal boes & fixed affixes
    Added modal boxes that are openable from within an OptionBox, and added Type options to the affix-type OptionBoxes (Type-1, Type-2, Type-3).

--- ithkapp/src/App.vue
+++ ithkapp/src/App.vue
@@ -2,101 +2,108 @@
   <!--<img alt="Vue logo" src="./assets/logo.png">
   <MainBody/>-->
   <div id="content">
-  <h1>Ithkapp</h1>
-  <div class="section"> <!-- Section 1: Root, etc. -->
-    <OptionBox title="Root (Slot III)" code="root" @send-message="handleSendMessage" type="text"/>
-    <OptionBox title="Stem" :options='["Stem 0","Stem 1","Stem 2", "Stem 3"]' code="stem" @send-message="handleSendMessage"/>
-    <OptionBox title="Function" :options='{"STA":"Static (STA)","DYN":"Dynamic (DYN)"}' code="func" @send-message="handleSendMessage"/>
-    <OptionBox title="Specification" :options='{"BSC":"Basic (BSC)","CTE":"Contential (CTE)","CSV":"Constitutive (CSV)","OBJ":"Objective (OBJ)"}' code="spec" @send-message="handleSendMessage"/>
-    <OptionBox title="Version" :options='{"PRC":"Processual","CPT":"Completive"}' code="ver" @send-message="handleSendMessage"/>
-  </div>
-  <div class="section"> <!-- Section 2: Concatenation & Affixes-->
-    <OptionBox title="Concatenation" :options='["None","Type 1","Type 2"]' code="concat" @send-message="handleSendMessage"/>
-    <OptionBox title="Slot V Affixes" code="5afx" @send-message="handleSendMessage" type="affix"/>
-    <OptionBox title="Slot VII Affixes" code="7afx" @send-message="handleSendMessage" type="affix"/>
-  </div>
-  <div class="section"> <!-- Section 3: Configuration -->
-    <h2 style="width:100%;">Configuration</h2>
-    <OptionBox title="Plexity" :options='{"U":"Uniplex","D":"Duplex","M":"Multiplex"}' code="plex" @send-message="handleSendMessage"/>
-    <OptionBox title="Similarity" :options='{"S":"Similar","D":"Dissimilar","F":"Fuzzy"}' code="simil" @send-message="handleSendMessage"/>
-    <OptionBox title="Connectedness" :options='{"S":"Separate","C":"Connected","F":"Fused"}' code="cctd" @send-message="handleSendMessage"/>
-  </div>
-  <div class="section"> <!-- Section 4: Slot VI -->
-    <OptionBox title="Affiliation" :options='{"CSL":"Consolidative","ASO":"Associative","COA":"Coalescent","VAR":"Variative"}' code="affil" @send-message="handleSendMessage"/>
-    <OptionBox title="Extension" :options='{"DEL":"Delimitive","PRX":"Proximal","ICP":"Inceptive","ATV":"Attentuative","GRA":"Graduative","DPL":"Depletive"}' code="ext" @send-message="handleSendMessage"/>
-    <OptionBox title="Perspective" :options='{"M":"Monadic","G":"Agglomerative","N":"Nomic","A":"Abstract"}' code="persp" @send-message="handleSendMessage"/>
-    <OptionBox title="Essence" :options='{"NRM":"Normal","RPV":"Representative"}' code="ess" @send-message="handleSendMessage"/>
-  </div>
-  <div class="section"> <!-- Section 5: Slot VIII -->
-    <OptionBox title="Vₙ" :options='{"val":"Valence","pha":"Phase","eff":"Effect","lvl":"Level","asp":"Aspect"}' code="vn" @send-message="handleSendMessage"/>
-    <OptionBox title="Valence" :options='{"MNO":"MNO","PRL":"PRL","CRO":"CRO","RCP":"RCP","CPL":"CPL","DUP":"DUP","DEM":"DEM","CNG":"CNG","PTI":"PTI"}' code="val" @send-message="handleSendMessage"/>
-    <OptionBox title="Phase" :options='{"PCT":"PCT","ITR":"ITR","REP":"REP","ITM":"ITM","RCT":"RCT","FRE":"FRE","FRG":"FRG","VAC":"VAC","FLC":"FLC"}' code="pha" @send-message="handleSendMessage"/>
-    <OptionBox title="Effect" :options='{"1:BEN":"1:BEN","2:BEN":"2:BEN","3:BEN":"3:BEN","SLF:BEN":"SLF:BEN","UNK":"UNK","SLF:DET":"SLF:DET","3:DET":"3:DET","2:DET":"2:DET","1:DET":"1:DET"}' code="eff" @send-message="handleSendMessage"/>
-    <OptionBox title="Level" :options='{"MIN":"MIN","SBE":"SBE","IFR":"IFR","DFT":"DFT","EQU":"EQU","SUR":"SUR","SPL":"SPL","SPQ":"SPQ","MAX":"MAX"}' code="lvl" @send-message="handleSendMessage"/>
-    <OptionBox title="Aspect" code="asp" @send-message="handleSendMessage"
-    :options = '{"RTR":"Retrospective",
-                "PRS":"Prospective",
-                "HAB":"Habitual",
-                "PRG":"Progressive",
-                "IMM":"Imminent",
-                "PCS":"Precessive",
-                "REG":"Regulative",
-                "SMM":"Summative",
-                "ATP":"Anticipatory",
-                "RSM":"Resumptive",
-                "CSS":"Cessative",
-                "PAU":"Pausal",
-                "RGR":"Regressive",
-                "PCL":"Preclusive",
-                "CNT":"Continuative",
-                "ICS":"Incessative",
-                "EXP":"Experiential",
-                "IRP":"Interruptive",
-                "PMP":"Preemptive",
-                "CLM":"Climactic",
-                "DLT":"Dilatory",
-                "TMP":"Temporary",
-                "XPD":"Expenditive",
-                "LIM":"Limitative",
-                "EPD":"Expeditive",
-                "PTC":"Protractive",
-                "PPR":"Preparatory",
-                "DCL":"Disclusive",
-                "CCL":"Conclusive",
-                "CUL":"Culminative",
-                "IMD":"Intermediative",
-                "TRD":"Tardative",
-                "TNS":"Transitional",
-                "ITC":"Intercommutative",
-                "MTV":"Motive",
-                "SQN":"Sequential"}'/>
-    <!-- Note to self: add a v-if & v-else regarding the following two, based on slot X -->
-    <OptionBox title="Mood" :options='{"FAC":"FAC","SUB":"SUB","ASM":"ASM","SPC":"SPC","COU":"COU","HYP":"HYP"}' code="mood" @send-message="handleSendMessage"/>
-    <OptionBox title="Case-Scope" :options='{"CCN":"Natural","CCA":"Antecedent","CCS":"Subaltern","CCQ":"Qualifier","CCP":"Precedent","CCV":"Successive"}' code="casc" @send-message="handleSendMessage"/>
-  </div>
-  <div class="section"> <!-- Section 6: Case -->
-    <OptionBox title="Case Category" :options='{"c0":"Transrelative","c1":"Appositive","c2":"Associative","c3":"Adverbial","c4":"Relational","c5":"Affinitive","c6":"Spatio-Temporal I","c7":"Spatio-Temporal II"}' code="ccat" @send-message="handleSendMessage"/>
-    <!-- Note to self: I need to bugfix the fact that switching case category resets the selected case but doesn't update the variables -->
-    <OptionBox v-if="this.ccat == 'c0'" title="Transrelative" :options='{"THM":"Thematic","INS":"Instrumental","ABS":"Absolutive","AFF":"Affective","STM":"Stimulative","EFF":"Effectuative","ERG":"Ergative","DAT":"Dative","IND":"Inducive"}' code="c" @send-message="handleSendMessage" />
-    <OptionBox v-if="this.ccat == 'c1'" title="Appositive" :options='{"POS":"Possesive","PRP":"Proprietive","GEN":"Genitive","ATT":"Attributive","PDC":"Productive","ITP":"Interpretative","OGN":"Originative","IDP":"Interdependent","PAR":"Partitive"}' code="c" @send-message="handleSendMessage" />
-  </div>
-  <p>Testing sections below:</p>
-  <input type="checkbox" v-model="test"/>
-  <div style="display: flex; flex-flow: row wrap; justify-content: center;">
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="3" @send-message="handleSendMessage" disabled/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="4" @send-message="handleSendMessage"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="5" @send-message="handleSendMessage"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="6" @send-message="handleSendMessage"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="7" @send-message="handleSendMessage"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="8" @send-message="handleSendMessage"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="9" @send-message="handleSendMessage" :disabled="test"/>
-    <OptionBox title="aaa" :options='["test","test2","test3"]' code="10" @send-message="handleSendMessage"/>
-  </div>
-  <!--(Note: The affix slots & root slot will eventually be modified to be a definition-based selector)-->
+    <h1>Ithkapp</h1>
+    <div class="section"> <!-- Section 1: Root, etc. -->
+      <OptionBox title="Root (Slot III)" code="root" @send-message="handleSendMessage" type="text" @modal="openModal" ref="root"/>
+      <OptionBox title="Stem" :options='["Stem 0","Stem 1","Stem 2", "Stem 3"]' code="stem" @send-message="handleSendMessage"/>
+      <OptionBox title="Function" :options='{"STA":"Static","DYN":"Dynamic"}' code="func" @send-message="handleSendMessage" @modal="openModal" ref="func"/>
+      <OptionBox title="Specification" :options='{"BSC":"Basic","CTE":"Contential","CSV":"Constitutive","OBJ":"Objective"}' code="spec" @send-message="handleSendMessage"/>
+      <OptionBox title="Version" :options='{"PRC":"Processual","CPT":"Completive"}' code="ver" @send-message="handleSendMessage"/>
+    </div>
+    <div class="section"> <!-- Section 2: Concatenation & Affixes-->
+      <OptionBox title="Concatenation" :options='["None","Type 1","Type 2"]' code="concat" @send-message="handleSendMessage"/>
+      <OptionBox title="Shortcutting" :options="['None','Yes']" code="shcut" @send-message="handleSendMessage"/>
+      <OptionBox title="Slot V Affixes" code="5afx" @send-message="handleSendMessage" type="affix"/>
+      <OptionBox title="Slot VII Affixes" code="7afx" @send-message="handleSendMessage" type="affix"/>
+    </div>
+    <div class="section"> <!-- Section 3: Configuration -->
+      <h2 style="width:100%;">Configuration</h2>
+      <OptionBox title="Plexity" :options='{"U":"Uniplex","D":"Duplex","M":"Multiplex"}' code="plex" @send-message="handleSendMessage"/>
+      <OptionBox title="Similarity" :options='{"S":"Similar","D":"Dissimilar","F":"Fuzzy"}' code="simil" @send-message="handleSendMessage"/>
+      <OptionBox title="Connectedness" :options='{"S":"Separate","C":"Connected","F":"Fused"}' code="cctd" @send-message="handleSendMessage"/>
+    </div>
+    <div class="section"> <!-- Section 4: Slot VI -->
+      <OptionBox title="Affiliation" :options='{"CSL":"Consolidative","ASO":"Associative","COA":"Coalescent","VAR":"Variative"}' code="affil" @send-message="handleSendMessage"/>
+      <OptionBox title="Extension" :options='{"DEL":"Delimitive","PRX":"Proximal","ICP":"Inceptive","ATV":"Attentuative","GRA":"Graduative","DPL":"Depletive"}' code="ext" @send-message="handleSendMessage"/>
+      <OptionBox title="Perspective" :options='{"M":"Monadic","G":"Agglomerative","N":"Nomic","A":"Abstract"}' code="persp" @send-message="handleSendMessage"/>
+      <OptionBox title="Essence" :options='{"NRM":"Normal","RPV":"Representative"}' code="ess" @send-message="handleSendMessage"/>
+    </div>
+    <div class="section"> <!-- Section 5: Slot VIII -->
+      <OptionBox title="Vₙ" :options='{"val":"Valence","pha":"Phase","eff":"Effect","lvl":"Level","asp":"Aspect"}' code="vn" @send-message="handleSendMessage"/>
+      <OptionBox title="Valence" :options='{"MNO":"MNO","PRL":"PRL","CRO":"CRO","RCP":"RCP","CPL":"CPL","DUP":"DUP","DEM":"DEM","CNG":"CNG","PTI":"PTI"}' code="val" @send-message="handleSendMessage"/>
+      <OptionBox title="Phase" :options='{"PCT":"PCT","ITR":"ITR","REP":"REP","ITM":"ITM","RCT":"RCT","FRE":"FRE","FRG":"FRG","VAC":"VAC","FLC":"FLC"}' code="pha" @send-message="handleSendMessage"/>
+      <OptionBox title="Effect" :options='{"1:BEN":"1:BEN","2:BEN":"2:BEN","3:BEN":"3:BEN","SLF:BEN":"SLF:BEN","UNK":"UNK","SLF:DET":"SLF:DET","3:DET":"3:DET","2:DET":"2:DET","1:DET":"1:DET"}' code="eff" @send-message="handleSendMessage"/>
+      <OptionBox title="Level" :options='{"MIN":"MIN","SBE":"SBE","IFR":"IFR","DFT":"DFT","EQU":"EQU","SUR":"SUR","SPL":"SPL","SPQ":"SPQ","MAX":"MAX"}' code="lvl" @send-message="handleSendMessage"/>
+      <OptionBox title="Aspect" code="asp" @send-message="handleSendMessage"
+      :options = '{"RTR":"Retrospective",
+                  "PRS":"Prospective",
+                  "HAB":"Habitual",
+                  "PRG":"Progressive",
+                  "IMM":"Imminent",
+                  "PCS":"Precessive",
+                  "REG":"Regulative",
+                  "SMM":"Summative",
+                  "ATP":"Anticipatory",
+                  "RSM":"Resumptive",
+                  "CSS":"Cessative",
+                  "PAU":"Pausal",
+                  "RGR":"Regressive",
+                  "PCL":"Preclusive",
+                  "CNT":"Continuative",
+                  "ICS":"Incessative",
+                  "EXP":"Experiential",
+                  "IRP":"Interruptive",
+                  "PMP":"Preemptive",
+                  "CLM":"Climactic",
+                  "DLT":"Dilatory",
+                  "TMP":"Temporary",
+                  "XPD":"Expenditive",
+                  "LIM":"Limitative",
+                  "EPD":"Expeditive",
+                  "PTC":"Protractive",
+                  "PPR":"Preparatory",
+                  "DCL":"Disclusive",
+                  "CCL":"Conclusive",
+                  "CUL":"Culminative",
+                  "IMD":"Intermediative",
+                  "TRD":"Tardative",
+                  "TNS":"Transitional",
+                  "ITC":"Intercommutative",
+                  "MTV":"Motive",
+                  "SQN":"Sequential"}'/>
+      <!-- Note to self: add a v-if & v-else regarding the following two, based on slot X -->
+      <OptionBox title="Mood" :options='{"FAC":"FAC","SUB":"SUB","ASM":"ASM","SPC":"SPC","COU":"COU","HYP":"HYP"}' code="mood" @send-message="handleSendMessage"/>
+      <OptionBox title="Case-Scope" :options='{"CCN":"Natural","CCA":"Antecedent","CCS":"Subaltern","CCQ":"Qualifier","CCP":"Precedent","CCV":"Successive"}' code="casc" @send-message="handleSendMessage"/>
+    </div>
+    <div class="section"> <!-- Section 6: Case -->
+      <OptionBox title="Case Category" :options='{"c0":"Transrelative","c1":"Appositive","c2":"Associative","c3":"Adverbial","c4":"Relational","c5":"Affinitive","c6":"Spatio-Temporal I","c7":"Spatio-Temporal II"}' code="ccat" @send-message="handleSendMessage"/>
+      <!-- Note to self: I need to bugfix the fact that switching case category resets the selected case but doesn't update the variables -->
+      <OptionBox v-if="this.ccat == 'c0'" title="Transrelative" :options='{"THM":"Thematic","INS":"Instrumental","ABS":"Absolutive","AFF":"Affective","STM":"Stimulative","EFF":"Effectuative","ERG":"Ergative","DAT":"Dative","IND":"Inducive"}' code="c" @send-message="handleSendMessage" />
+      <OptionBox v-if="this.ccat == 'c1'" title="Appositive" :options='{"POS":"Possesive","PRP":"Proprietive","GEN":"Genitive","ATT":"Attributive","PDC":"Productive","ITP":"Interpretative","OGN":"Originative","IDP":"Interdependent","PAR":"Partitive"}' code="c" @send-message="handleSendMessage" />
+    </div>
+    <p>Testing sections below:</p>
+    <input type="checkbox" v-model="test"/>
+    <div style="display: flex; flex-flow: row wrap; justify-content: center;">
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="3" @send-message="handleSendMessage" disabled/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="4" @send-message="handleSendMessage"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="5" @send-message="handleSendMessage"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="6" @send-message="handleSendMessage"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="7" @send-message="handleSendMessage"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="8" @send-message="handleSendMessage"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="9" @send-message="handleSendMessage" :disabled="test"/>
+      <OptionBox title="aaa" :options='["test","test2","test3"]' code="10" @send-message="handleSendMessage"/>
+    </div>
+    <!--(Note: The affix slots & root slot will eventually be modified to be a definition-based selector)-->
   </div>
   <div id="footer">
     <p>{{ithkword.join("")}} /{{ipa}}/</p>
     <p>{{gloss}}</p>
+  </div>
+  <div id="modal" class="modal" @click.self="closeModal()">
+    <div class="modal-content">
+      <span class="close" @click="closeModal()">&times;</span>
+      <p>Hi from {{modalContent}}</p>
+    </div>
   </div>
 </template>
 
@@ -111,13 +118,14 @@ export default {
   data() {
     return {
       test: false,
+      modalContent: "",
       // the following variables correspond to things visible on-screen
       ithkword: ["a","b","c","","","","","",""], // ithkuil word, split into slots (0-8 = I-IX)
       grammaroptions: { // grammar options
         "root":"",
         "5afx":[],
         "7afx":[],
-        "concat":"None",
+        "concat":0,
         
       },
       gloss: "al.FA.bet", // gloss of word
@@ -131,6 +139,16 @@ export default {
       if (code=="ccat") {
         this.ccat = value;
       }
+    },
+    openModal(code) {
+      console.log("Modal opening for",code);
+      this.modalContent = code;
+      /*console.log(this.$refs[code]);
+      this.$refs[code].updateValue("DYN");*/
+      document.getElementById("modal").style.display = "block";
+    },
+    closeModal() {
+      document.getElementById("modal").style.display = "none";
     },
     calculateSlot1() {
       console.log("AAAAA");
@@ -177,5 +195,37 @@ export default {
   border-width: 2px;
   padding: 2px;
   margin: 10px;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+}
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
--- ithkapp/src/components/optionbox.vue
+++ ithkapp/src/components/optionbox.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="optionbox" :class="OBclass">
+    <button style="float:right; margin-top: 5px; margin-right: 5px; display:inline" @click="this.$emit('modal',code)">?</button>
     <h3>{{title}}</h3>
 
     <!-- The following depends on which TYPE of grammar option this is: -->
@@ -9,20 +10,26 @@
 
     <!-- If it's a list of text entries: -->
     <div v-else-if="type=='affix'">
-      <div v-for="(affix,index) in affixes" :key="affix"> <!-- For each affix/degree pair in the array "affixes", add a textbox and dropdown linked to each-->
-        <input v-model="affixes[index][0]" @input="this.$emit('send-message',affixes,code)" placeholder="Enter..." maxlength=3/>
-        <select v-model="affixes[index][1]" @input="this.$emit('send-message',affixes,code)" style="display:inline-block">
-          <option v-for="num in Array(10).keys()" :key="num">{{num}}</option>
+      <div v-for="(affix,index) in affixes" :key="affix"> <!-- For each affix/degree pair in the array "affixes", add a textbox and two dropdowns linked to each-->
+        <input v-model="affixes[index][0]" @input="this.$emit('send-message',affixes,code)" placeholder="Enter..." maxlength=3/> <!-- Textbox -->
+        <select v-model="affixes[index][1]" @input="this.$emit('send-message',affixes,code)" style="display:inline-block"> <!-- Dropdown 1 (Degree) -->
+          <option v-for="num in Array(10).keys()" :key="(num+1)%10" :value="(num+1)%10">Deg. {{(num+1)%10}}</option>
+        </select>
+        <select v-model="affixes[index][2]" @input="this.$emit('send-message',affixes,code)" style="display:inline-block"> <!-- Dropdown 2 (Type) -->
+          <option :value="1">Type-1</option>
+          <option :value="2">Type-2</option>
+          <option :value="3">Type-3</option>
         </select>
       </div>
-      <input type="button" value="Add" @click="affixes.push(['',0]); this.$emit('send-message',affixes,code)"/>
-      <input type="button" value="Remove" @click="affixes.pop(); this.$emit('send-message',affixes,code)"/>
+      <input type="button" value="Add" @click="affixes.push(['',1,1]); this.$emit('send-message',affixes,code)"/> <!-- Button to add another affix -->
+      <input type="button" value="Remove" @click="affixes.pop(); this.$emit('send-message',affixes,code)"/> <!-- Button to remove an affix -->
     </div>
 
     <!-- Otherwise, assume it's a dropdown list: -->
     <select v-else v-model="option" @change="this.$emit('send-message',option,code)" :disabled="disabled" :id="code">
       <!--<option selected disabled>Select...</option>-->
-      <option v-for="(opt, short) in options" :key="opt" :value="short">{{opt}}</option>
+      <option v-for="(opt, short) in options" :key="opt" :value="short">{{opt}} {{short === short.toString().toUpperCase() ? "("+short+")" : ""}}</option>
+      <!-- above: show the long form of the option (opt), and IF the short form (short) is uppercase then show that in brackets as well -->
     </select>
 
     <p v-if='this.$props.type == "affix" && this.affixes.length != 0 && !this.affixes.every(function (e) {return e[0] != ""})'><b>ERROR:</b> Empty affixes</p> <!-- if there's an error, have text that says so -->
@@ -32,6 +39,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'OptionBox',
   props: {
@@ -59,6 +67,14 @@ export default {
   mounted() {
     if (this.$props.type != "affix" && this.$props.type != "text"){
       document.getElementById(this.$props.code).selectedIndex = 0;
+    }
+  },
+  methods: {
+    updateValue(toUpdate) {
+      if (this.$props.type != "affix" && this.$props.type != "text" && toUpdate in this.$props.options) {
+        this.option = toUpdate;
+        this.$emit('send-message',this.option,this.$props.code)
+      }
     }
   }
 }
