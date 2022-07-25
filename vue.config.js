12ad029aa8ac7cf9cd0c74c7251cb44ab318f1cd
Nick L <44630816+TheXXOs@users.noreply.github.com>
2022-07-25 19:11:52 +0930

    Instructions for vue.config.js
    I'll probably add this to a README or something later too

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ithkapp/'
+    // replace './' with '/ithkapp/' for building for the github sites
+    // replace './' with '/<path>/' for hosting at https://example.com/<path>/, or just '/' if hosting at the root
+    // alteratively, leave it as './' for building to local files
+    ? './'
     : '/'
 })
