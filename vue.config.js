f5ac4eae1b6836dee2e50411cf472728b8e08726
Nick L <44630816+TheXXOs@users.noreply.github.com>
2022-08-02 19:00:28 +0930

    New favicon
    the old one was just an example character from the script document, this new one actually means "hwirbuvie"

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    // replace './' with '/ithkapp/' for building for the github sites
-    // replace './' with '/<path>/' for hosting at https://example.com/<path>/, or just '/' if hosting at the root
-    // alteratively, leave it as './' for building to local files
-    ? './'
+    // replace '/ithkapp/' with './' for building to local files
+    // replace '/ithkapp/' with '/<path>/' for hosting at https://example.com/<path>/, or just '/' if hosting at the root
+    // alteratively, leave it as '/ithkapp/' for building for the github sites
+    ? '/ithkapp/'
     : '/'
 })
